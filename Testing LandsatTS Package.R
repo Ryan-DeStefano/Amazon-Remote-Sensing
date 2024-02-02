8c7a0cb9a7025012957237f315788e7a5c6493e6
Ryan DeStefano <129787104+Ryan-DeStefano@users.noreply.github.com>
2024-02-02 10:22:22 -0800

    Update Testing LandsatTS Package.R

--- Testing LandsatTS Package.R
+++ Testing LandsatTS Package.R
@@ -41,9 +41,6 @@ n.pts <- 15
 test.pts.sf <- st_sample(x=test_poly_sf, size=n.pts) %>% st_sf()
 test.pts.sf$sample_id <- paste0('S_', 1:n.pts)
 
-# Or use lsat_get_pixel_centers to get all pixels within the polygon
-pixel_list_test_poly <- lsat_get_pixel_centers(test_poly_sf, plot_map = TRUE)
-
 # Plot polygon with sample points
 leaflet() %>%
   addProviderTiles('Esri.WorldImagery') %>%
@@ -58,6 +55,9 @@ leaflet() %>%
               color='white',
               weight=3) %>%
   addScaleBar(options=scaleBarOptions(imperial=F))
+
+# Or use lsat_get_pixel_centers to get all pixels within the polygon
+pixel_list_test_poly <- lsat_get_pixel_centers(test_poly_sf, plot_map = TRUE)
 
 # Export time-series using lsat_export_ts(), will be saved to a lsat folder in google drive
 task_list <- lsat_export_ts(test.pts.sf)
@@ -91,11 +91,47 @@ lsat.dt <- lsat_calibrate_rf(lsat.dt,
                              write.output = F)
 
 # If needed, then manually overwrite the uncalibrated data with the calibrated data, then drop the duplicate column 
-# lsat.dt <- lsat.dt[, ndvi := ndvi.xcal]
-# lsat.dt <- lsat.dt[, ndvi.xcal := NULL)
+#lsat.dt <- lsat.dt[, ndvi := ndvi.xcal]
+#lsat.dt <- lsat.dt[, ndvi := NULL]
 
+# Fit phenological models (cubic splines) to each time series (Took out vi.min arg)
+lsat.pheno.dt <- lsat_fit_phenological_curves(lsat.dt, si = 'ndvi', window.yrs = 5, window.min.obs = 10, spl.fit.outfile = F, progress = T)
 
+# Gives some summary stats on the phenological curve fits
+lsat.gs.dt <- lsat_summarize_growing_seasons(lsat.pheno.dt, si = 'ndvi', min.frac.of.max = 0.75)
 
+#Creates boxplots for num of Obs vs % dif from max observed NDVI
+lsat.gs.eval.dt <- lsat_evaluate_phenological_max(lsat.pheno.dt, si = 'ndvi', min.obs = 10, reps = 5, min.frac.of.max = 0.75, outdir = NA)
 
+# Create data for and plot histogram of relative change in greeness
+lsat.trnds.dt <- lsat_calc_trend(lsat.gs.dt, si = 'ndvi.max', 2000:2020, sig = 0.1)
+lsat_plot_trend_hist(lsat.trnds.dt, xlim=c(-21,21))
 
+# Create leaflet plot with trends imposed
+colors.dt <- data.table(trend.cat=c("greening","no_trend","browning"),
+                        trend.color=c("springgreen","white","orange"))
 
+lsat.trnds.dt <- lsat.trnds.dt[colors.dt, on='trend.cat']
+
+lsat.trnds.dt <- na.omit(lsat.trnds.dt)
+
+lsat.trnds.sf <- st_as_sf(lsat.trnds.dt,
+                            coords=c("longitude", "latitude"),
+                            crs=4326)
+
+leaflet() %>%
+  addProviderTiles('Esri.WorldImagery') %>%
+  addPolylines(data=test_poly_sf, color='white', weight=3) %>%
+  addCircleMarkers(data=lsat.trnds.sf,
+                   color='white',
+                   weight=1,
+                   opacity=0.9,
+                   fillColor=~trend.color,
+                   fillOpacity=0.5,
+                   radius=~sqrt(abs(total.change.pcnt))*3) %>%
+  setView(-160, 68, zoom=7) %>%
+  addLegend('bottomright',
+            colors=colors.dt$trend.color,
+            labels=colors.dt$trend.cat,
+            title='NDVImax trend',
+            opacity=1)
