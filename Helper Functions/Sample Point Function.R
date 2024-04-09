7aa526c687f02efa91e969b703de302e4e5cbac3
Ryan DeStefano <rddestef@calpoly.edu>
2024-04-09 13:01:06 -0700

    Aggregated the sample point function into one function. Minor changes to the peru landsat app file (not important)

--- Helper Functions/Sample Point Function.R
+++ Helper Functions/Sample Point Function.R
@@ -1,22 +1,20 @@
 num_points_vector <- c()
 
 for (i in 1:nrow(top_10_para)){
-  
   estimated <- top_10_para$estimated_points[i]
   
   if (estimated < 10){
     num_points <- 1
   }
-  
-  else if (estimated > 100){
-    num_points <- min(20, as.integer(.01*estimated))
+  else if (estimated > 200){
+    num_points <- 20
   }
-  
   else {
-    num_points <- max(1, as.integer(estimated*.01))
+    num_points <- max(1, as.integer(estimated*.1))
   }
   
   num_points_vector <- c(num_points_vector, num_points)
-  
 }
 
+estmiated_hours_to_export <- sum(num_points_vector)/2/60
+estmiated_hours_to_export
