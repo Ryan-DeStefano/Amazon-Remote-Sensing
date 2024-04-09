7c384cce53b7bff01ffaaa3d3d49434d97c848bf
Ryan DeStefano <rddestef@calpoly.edu>
2024-04-09 14:06:57 -0700

    Formatting Update

--- Helper Functions/Sample Point Function.R
+++ Helper Functions/Sample Point Function.R
@@ -1,20 +1,30 @@
-num_points_vector <- c()
-
-for (i in 1:nrow(top_10_para)){
-  estimated <- top_10_para$estimated_points[i]
+num_sample_points <- function(dataset){ 
   
-  if (estimated < 10){
-    num_points <- 1
-  }
-  else if (estimated > 200){
-    num_points <- 20
-  }
-  else {
-    num_points <- max(1, as.integer(estimated*.1))
+  num_points_vector <- c()
+  
+  for (i in 1:nrow(dataset)){
+    estimated <- dataset$estimated_points[i]
+    
+    if (estimated < 10){
+      num_points <- 1
+    }
+    else if (estimated > 200){
+      num_points <- 20
+    }
+    else {
+      num_points <- max(1, as.integer(estimated*.1))
+    }
+  
+    num_points_vector <- c(num_points_vector, num_points)
   }
   
-  num_points_vector <- c(num_points_vector, num_points)
+  return(num_points_vector)
 }
 
-estmiated_hours_to_export <- sum(num_points_vector)/2/60
+# Brazil
+estmiated_hours_to_export <- sum(num_sample_points(top_10_para))/2/60
+estmiated_hours_to_export
+
+#Peru
+estmiated_hours_to_export <- sum(num_sample_points(top_10_peru))/2/60
 estmiated_hours_to_export
